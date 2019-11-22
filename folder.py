d15f58000483851479780215e15026ad157728a0
samwieske <57816684+samwieske@users.noreply.github.com>
2019-11-22 15:20:21 -0800

    Update folder.py

--- folder.py
+++ folder.py
@@ -34,11 +34,69 @@ Table = os.path.join(folder_path, "city.dbf")
 
 #Step 1. Clip Places to Region
 
+#Define raster extent output
+rasPoly = os.path.join(folder_path, "rasterpoly.shp")
+
+#The raster datasets in the input workspace
+in_raster_datasets = arcpy.ListRasters()
+
+#sr = arcpy.SpatialReference(r"C:\Users\Fuaad Khan\Desktop\Lab3\LabData\LabData.mdx")
+#Create a Polygon Feature Class
+arcpy.CreateFeatureclass_management(os.path.dirname(rasPoly),
+                                   os.path.basename(rasPoly),
+                                   "POLYGON", spatial_reference = Region
+arcpy.AddField_management(rasPoly,"RasterName", "String","","",250)
+arcpy.AddField_management(rasPoly,"RasterPath", "String","","",250)
+
+cursor = arcpy.InsertCursor(rasPoly)
+point = arcpy.Point()
+array = arcpy.Array()
+corners = ["lowerLeft", "lowerRight", "upperRight", "upperLeft"]
+for Ras in in_raster_datasets:
+    feat = cursor.newRow()  
+    r = arcpy.Raster(Ras)
+    for corner in corners:    
+        point.X = getattr(r.extent, "%s" % corner).X
+        point.Y = getattr(r.extent, "%s" % corner).Y
+        array.add(point)
+    array.add(array.getObject(0))
+    polygon = arcpy.Polygon(array)
+    feat.shape = polygon
+    feat.setValue("RasterName", Ras)
+    feat.setValue("RasterPath", folder_path + "\\" + Ras)
+    cursor.insertRow(feat)
+    array.removeAll()
+del feat
+del cursor 
+
+                                    
+                                    
+#Run Clip Analysis
+
+in_features = Places
+clip_features = rasPoly
+clipCity = "clipCities.shp"
+
+arcpy.Clip_analysis(in_features, clip_features, clipCity)                                    
+                                    
+                                    
 #Step 2. Define Raster values
     #2A. Create NDVI to determine a vegetation scale
-    #2B. Create Elevation values map
+                                    
+if arcpy.CheckOutExtension("Spatial") == "CheckedOut":
+	RedBandF=arcpy.Raster(r'C:\Users\samwieske\Desktop\LC08_L1TP_041036_20191021_20191030_01_T1\LC08_L1TP_041036_20191021_20191030_01_T1_B4.tif')
+	InfraredBandF=arcpy.Raster(r'C:\Users\samwieske\Desktop\LC08_L1TP_041036_20191021_20191030_01_T1\LC08_L1TP_041036_20191021_20191030_01_T1_B5.tif')
+	RedBand=arcpy.sa.Float(RedBandF) #Must be redefined as floats, not integers
+	RedBand.save("RedBand.TIF")
+	InfraredBand=arcpy.sa.Float(InfraredBandF)
+	InfraredBand.save("InfraredBand.TIF")
+	output_raster = (InfraredBand - RedBand) / (RedBand + InfraredBand) #NDVI calculation
+	output_raster.save("NDVI.TIF")
 
-#Step3. Reclassify tool
+print "NDVI raster has been successfully computed."
+                                    
+
+
 
 #Step 4. Buffer around cities
 
