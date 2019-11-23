5080e209f7bdc38b113e9b7634e2b356a55561f7
samwieske <57816684+samwieske@users.noreply.github.com>
2019-11-22 16:01:23 -0800

    Update folder.py

--- folder.py
+++ folder.py
@@ -1,5 +1,5 @@
 #Connect to folder
-folder_path = r'C:\Users\samwieske\Desktop\FinalProjectfolder - Copy'
+folder_path = r'C:\Users\samwieske\Desktop\folderpath'
 
 #import the many modules I will be using 
 import arcpy
@@ -16,35 +16,26 @@ arcpy.env.overwriteOutput = True
 #Vector data of cities or regions for buffer analysis
 
 #Places = GetParameterAsText(0)
-Places = r'C:\Users\samwieske\Desktop\FinalProjectfolder - Copy\PopulatedPlaces'
+Places = r"C:\Users\samwieske\Desktop\folderpath\ne_10m_populated_places.shp"
 
 #Raster DEM data for elevation analysis
 #Region = GetParameterAsText(1)
-Region = r'C:\Users\samwieske\Desktop\FinalProjectfolder - Copy\LosAngelesLandsat8'
+Region = folder_path + "\LC08_L1TP_041036_20191021_20191030_01_T1_B1.tif"
 
 
-#Optional: Demographic data - based on real estate property values (TIGER/LINE data)
-#Optional: FIRMS - can convert vector to raster
-
-
-#Define Outputs
-#IdealCity= GetParameterAsText(2)
-IdealCity = os.path.join(folder_path, "city.shp")
-Table = os.path.join(folder_path, "city.dbf")
 
 #Step 1. Clip Places to Region
 
 #Define raster extent output
-rasPoly = os.path.join(folder_path, "rasterpoly.shp")
+rasPoly = r"C:\Users\samwieske\Desktop\folderpath\rasterpoly.shp"
 
 #The raster datasets in the input workspace
 in_raster_datasets = arcpy.ListRasters()
 
 #sr = arcpy.SpatialReference(r"C:\Users\Fuaad Khan\Desktop\Lab3\LabData\LabData.mdx")
 #Create a Polygon Feature Class
-arcpy.CreateFeatureclass_management(os.path.dirname(rasPoly),
-                                   os.path.basename(rasPoly),
-                                   "POLYGON", spatial_reference = Region
+arcpy.CreateFeatureclass_management(os.path.dirname(rasPoly),os.path.basename(rasPoly),"POLYGON", spatial_reference = Region)
+                                    
 arcpy.AddField_management(rasPoly,"RasterName", "String","","",250)
 arcpy.AddField_management(rasPoly,"RasterPath", "String","","",250)
 
@@ -69,8 +60,7 @@ for Ras in in_raster_datasets:
 del feat
 del cursor 
 
-                                    
-                                    
+                                                                   
 #Run Clip Analysis
 
 in_features = Places
@@ -84,8 +74,8 @@ arcpy.Clip_analysis(in_features, clip_features, clipCity)
     #2A. Create NDVI to determine a vegetation scale
                                     
 if arcpy.CheckOutExtension("Spatial") == "CheckedOut":
-	RedBandF=arcpy.Raster(r'C:\Users\samwieske\Desktop\LC08_L1TP_041036_20191021_20191030_01_T1\LC08_L1TP_041036_20191021_20191030_01_T1_B4.tif')
-	InfraredBandF=arcpy.Raster(r'C:\Users\samwieske\Desktop\LC08_L1TP_041036_20191021_20191030_01_T1\LC08_L1TP_041036_20191021_20191030_01_T1_B5.tif')
+	RedBandF=arcpy.Raster(folder_path + '\LC08_L1TP_041036_20191021_20191030_01_T1_B4.tif')
+	InfraredBandF=arcpy.Raster(folder_path + '\LC08_L1TP_041036_20191021_20191030_01_T1_B5.tif')
 	RedBand=arcpy.sa.Float(RedBandF) #Must be redefined as floats, not integers
 	RedBand.save("RedBand.TIF")
 	InfraredBand=arcpy.sa.Float(InfraredBandF)
@@ -95,7 +85,6 @@ if arcpy.CheckOutExtension("Spatial") == "CheckedOut":
 
 print "NDVI raster has been successfully computed."
                                     
-
 
 
 #Step 4. Buffer around cities
