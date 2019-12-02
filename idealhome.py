52f66865fab71a092ca7ba6074ad2a9888b608cb
samwieske <57816684+samwieske@users.noreply.github.com>
2019-12-01 17:57:11 -0800

    Update idealhome.py

--- idealhome.py
+++ idealhome.py
@@ -1,6 +1,8 @@
+#Authors: Fuaad Khan, Sam Wieske
 #Define working directory
-#folder_path = GetParameterAsText(0)
-folder_path = r'C:\Users\fuaadkhan\Documents\folderpath'
+#folder_path: Where the input and output data will be stored
+folder_path = arcpy.GetParameterAsText(0)
+#folder_path = r'C:\Users\Fuaad Khan\Desktop\final\folderpath'
 
 #Import Libraries
 import arcpy
@@ -14,45 +16,56 @@ arcpy.env.overwriteOutput = True
 
 #Define Inputs
 #Places: City Shapefile
-#Places = GetParameterAsText(1)
-Places = r"C:\Users\fuaadkhan\Documents\folderpath\ne_10m_populated_places.shp"
+Places = arcpy.GetParameterAsText(1)
+#Places = r"C:\Users\Fuaad Khan\Desktop\final\folderpath\LACN.shp"
 
 #Region: Landsat Raster of Los Angeles
-#Region = GetParameterAsText(2)
-Region = folder_path + "\LC08_L1TP_041036_20191021_20191030_01_T1_B1.tif"
+Region = arcpy.GetParameterAsText(2)
+#Region = folder_path + "\LC08_L1TP_041036_20191021_20191030_01_T1_B1.tif"
+
+#b4: Band 4 from the Landsat Raster
+b4 = arcpy.GetParameterAsText(3)
+
+#b5: Band 5 from the Landsat Raster
+b5 = arcpy.GetParameterAsText(4)
 
 #Schools: School points of Los Angeles
-#Schools = GetParameterAsText(3)
-Schools = folder_path + "\LMS_Data_Public_20160114.shp"
+Schools = arcpy.GetParameterAsText(5)
+#Schools = folder_path + "\LMS_Data_Public_20160114.shp"
 
-#LA Metro Stops
-#Metro = GetParameterAsText(3)
-metroStops = r'C:\Users\fuaadkhan\Desktop\LA__Transit_Stops\LA_Stops_Enriched.shp'
+#Bus Stops
+busStops = arcpy.GetParameterAsText(6)
+#busStops = r'C:\Users\Fuaad Khan\Desktop\final\folderpath\LA_Stops_Enriched.shp'
+
+#uBuffer: User's input buffer area
+uBuffer = arcpy.GetParameterAsText(7)
 
 #Step 1: NDVI
 if arcpy.CheckOutExtension("Spatial") == "CheckedOut":
-        RedBandF=arcpy.Raster(folder_path + '\LC08_L1TP_041036_20191021_20191030_01_T1_B4.tif')
-        InfraredBandF=arcpy.Raster(folder_path + '\LC08_L1TP_041036_20191021_20191030_01_T1_B5.tif')
-        RedBand=arcpy.sa.Float(RedBandF) #Must be redefined as floats, not integers
-        RedBand.save("RedBand.TIF")
-        InfraredBand=arcpy.sa.Float(InfraredBandF)
-        InfraredBand.save("InfraredBand.TIF")
-        output_raster = (InfraredBand - RedBand) / (RedBand + InfraredBand) #NDVI calculation
-        output_raster.save("NDVI.TIF")
+    RedBandF=arcpy.Raster(b4)
+    InfraredBandF=arcpy.Raster(b5)
+    RedBand=arcpy.sa.Float(RedBandF) #Must be redefined as floats, not integers
+    RedBand.save("RedBand.TIF")
+    InfraredBand=arcpy.sa.Float(InfraredBandF)
+    InfraredBand.save("InfraredBand.TIF")
+    output_raster = (InfraredBand - RedBand) / (RedBand + InfraredBand) #NDVI calculation
+    output_raster.save("NDVI.TIF")
 else:
-    print "Arc License is not avilable"
+    print("Arc License is not avilable")
 
     print("NDVI raster has been successfully computed.")
 
-#Step 2: 
+#Step 2: Create a polygon boundary for future clipping of vector data to the raster region
+
+#Check for input vector data type: Point or Polygon
 desc=arcpy.Describe(Places)
 if desc.shapeType == "Point": 
     print ("Shape Type: Point")
 
-#List of raster datasets in the input workspace
+    #List of raster datasets in the input workspace
     in_raster_datasets = arcpy.ListRasters()
 
-#Create a polygon feature class for raster boundary
+    #Create a polygon feature class for raster boundary
     arcpy.CreateFeatureclass_management(folder_path,"raspoly.shp","POLYGON", spatial_reference = Region)                                 
     arcpy.AddField_management("raspoly.shp","RasterName", "String","","",250)
     arcpy.AddField_management("raspoly.shp","RasterPath", "String","","",250)
@@ -80,98 +93,156 @@ if desc.shapeType == "Point":
 
     print("RasterPoly Created...")                                                                   
 
-#Run Clip Analysis
+    #Step 3: Run Clip Analysis
     in_features = Places
     clip_features = "raspoly.shp"
     clipCity = "clipCities.shp"
     arcpy.Clip_analysis(in_features, clip_features, clipCity)                                    
     print("Cities Clipped...")                                    
 
-#Step 4: 5 mile Buffer around cities and Clip Raster to Buffer Mask
+    #Step 4: 5 mile Buffer around cities and Clip Raster to Buffer Mask
     cityBuffer = "clipCities_Buffer.shp"
-    arcpy.Buffer_analysis(clipCity, cityBuffer, "3 Miles") #change this to clipCity instead of ClipCity
+    arcpy.Buffer_analysis(clipCity, cityBuffer, uBuffer) #change this to clipCity instead of ClipCity
     print("Buffer Created...")
 
-#Step 5: Zonal statistics 
+    #Step 5: Zonal statistics 
     outputTable = folder_path+"\NDVI_table.dbf"
     arcpy.gp.ZonalStatisticsAsTable_sa(cityBuffer, "NAME", "NDVI.TIF", outputTable, "DATA", "MEAN")
     print ("Zonal Statistics Table Created...")
 
-#School Count
+    #Step 6: School Count
     arcpy.SpatialJoin_analysis(cityBuffer, Schools, "SpatialJoin.shp", "JOIN_ONE_TO_MANY", "KEEP_ALL", "", "INTERSECT")
     arcpy.Statistics_analysis("SpatialJoin.shp", "schoolStats.dbf", "Join_Count SUM","NAME")
     print ("School Count DBF Created...")
 
-#Join schoolStats to NDVI Table
+    #Step 7: Join schoolStats to NDVI Table
     arcpy.JoinField_management(in_data="NDVI_table.dbf", in_field="NAME", join_table="schoolStats.dbf", join_field="name", fields="FREQUENCY")
     print ("School Count Joined to NDVI Table...")
     
-#Metro Count
-    arcpy.SpatialJoin_analysis(cityBuffer, metroStops, "MetroSpatialJoin.shp", "JOIN_ONE_TO_MANY", "KEEP_ALL", "", "INTERSECT")
-    arcpy.Statistics_analysis("MetroSpatialJoin.shp", "metroStats.dbf", "Join_Count SUM","NAME")
-    print ("Metro Count DBF Created...")
+    #Step 8: Bus Stop Count
+    arcpy.SpatialJoin_analysis(cityBuffer, busStops, "BusSpatialJoin.shp", "JOIN_ONE_TO_MANY", "KEEP_ALL", "", "INTERSECT")
+    arcpy.Statistics_analysis("BusSpatialJoin.shp", "BusStats.dbf", "Join_Count SUM","NAME")
+    print ("Bus Count DBF Created...")
 
-#Join metroStats to NDVI Table
-    arcpy.JoinField_management(in_data="NDVI_table.dbf", in_field="NAME", join_table="metroStats.dbf", join_field="name", fields="FREQUENCY")
-    print ("Metro Count Joined to NDVI Table...")
+    #Step 9: Join BusStats to NDVI Table
+    arcpy.JoinField_management(in_data="NDVI_table.dbf", in_field="NAME", join_table="BusStats.dbf", join_field="name", fields="FREQUENCY")
+    print ("Bus Count Joined to NDVI Table...")
 
     
 ############################################################################################################    
 elif desc.shapeType == "Polygon":
-        print("Shape Type: Polygon")
-        #NDVI -> Table
-        outputTable = folder_path + "\NDVITable.dbf"
-        arcpy.gp.ZonalStatisticsAsTable_sa(Places, "NAME", "NDVI.TIF", outputTable, "DATA", "MEAN")
-        print ("Zonal Statistics Table Created...")
+    print("Shape Type: Polygon")
+    #NDVI -> Table
+    outputTable = folder_path + "\NDVITable.dbf"
+    arcpy.gp.ZonalStatisticsAsTable_sa(Places, "NAME", "NDVI.TIF", outputTable, "DATA", "MEAN")
+    print ("Zonal Statistics Table Created...")
 
-        #School Count
-        arcpy.SpatialJoin_analysis(Places, Schools, "SpatialJoin.shp", "JOIN_ONE_TO_MANY", "KEEP_ALL", "", "INTERSECT")
-        arcpy.Statistics_analysis("SpatialJoin.shp", "schoolStats.dbf", "Join_Count SUM","NAME")
-        print ("School Count DBF Created...")
+    #School Count
+    arcpy.SpatialJoin_analysis(Places, Schools, "SpatialJoin.shp", "JOIN_ONE_TO_MANY", "KEEP_ALL", "", "INTERSECT")
+    arcpy.Statistics_analysis("SpatialJoin.shp", "schoolStats.dbf", "Join_Count SUM","NAME")
+    print ("School Count DBF Created...")
 
-        #Join schoolStats to NDVI Table
-        arcpy.JoinField_management(in_data="NDVITable.dbf", in_field="NAME", join_table="schoolStats.dbf", join_field="name", fields="FREQUENCY")
-        print ("School Count Joined to NDVI Table...")
+    #Join schoolStats to NDVI Table
+    arcpy.JoinField_management(in_data="NDVITable.dbf", in_field="NAME", join_table="schoolStats.dbf", join_field="name", fields="FREQUENCY")
+    print ("School Count Joined to NDVI Table...")
 
-        #Metro Count
-        arcpy.SpatialJoin_analysis(Places, metroStops, "MetroSpatialJoin.shp", "JOIN_ONE_TO_MANY", "KEEP_ALL", "", "INTERSECT")
-        arcpy.Statistics_analysis("MetroSpatialJoin.shp", "metroStats.dbf", "Join_Count SUM","NAME")
-        print ("Metro Count DBF Created...")
-        #Join metroStats to NDVI Table
-        arcpy.JoinField_management(in_data="NDVITable.dbf", in_field="NAME", join_table="metroStats.dbf", join_field="name", fields="FREQUENCY")
-        print ("Metro Count Joined to NDVI Table...")
+    #Bus Count
+    arcpy.SpatialJoin_analysis(Places, busStops, "BusSpatialJoin.shp", "JOIN_ONE_TO_MANY", "KEEP_ALL", "", "INTERSECT")
+    arcpy.Statistics_analysis("BusSpatialJoin.shp", "BusStats.dbf", "Join_Count SUM","NAME")
+    print ("Metro Count DBF Created...")
+    
+    #Join BusStats to NDVI Table
+    arcpy.JoinField_management(in_data="NDVITable.dbf", in_field="NAME", join_table="BusStats.dbf", join_field="name", fields="FREQUENCY")
+    print ("Bus Count Joined to NDVI Table...")
 
 else:
     print ("Invalid Input, shape type is: "+desc.shapeType(Places))
 
 arcpy.AddField_management("NDVITable.dbf", "NumBus", "FLOAT")
 arcpy.AddField_management("NDVITable.dbf", "NumSchools", "FLOAT")
-arcpy.AddField_management("NDVITable.dbf", "FinalScore", "FLOAT")
+#arcpy.AddField_management("NDVITable.dbf", "FinalScore", "FLOAT")
 
 arcpy.CalculateField_management("NDVITable.dbf", "NumBus", "!FREQUENCY! - 1","PYTHON")
 arcpy.CalculateField_management("NDVITable.dbf", "NumSchools", "!FREQUENC_1! - 1","PYTHON")
 
 arcpy.DeleteField_management("NDVITable.dbf", "FREQUENCY")
 arcpy.DeleteField_management("NDVITable.dbf", "FREQUENC_1")
+arcpy.DeleteField_management("NDVITable.dbf", "COUNT")
+arcpy.DeleteField_management("NDVITable.dbf", "ZONE_CODE")
 
-arcpy.AddField_management("NDVITable.dbf", "BusScore", "FLOAT")
-arcpy.AddField_management("NDVITable.dbf", "MeanScore", "FLOAT")
-arcpy.AddField_management("NDVITable.dbf", "SchoolScore", "FLOAT")
-arcpy.AddField_management("NDVITable.dbf", "FinalScore", "FLOAT")
+arcpy.AddField_management("NDVITable.dbf", "Bus_Scr", "FLOAT")
+arcpy.AddField_management("NDVITable.dbf", "Mean_Scr", "FLOAT")
+arcpy.AddField_management("NDVITable.dbf", "Schl_Scr", "FLOAT")
+arcpy.AddField_management("NDVITable.dbf", "Final_Scr", "FLOAT")
 
-meanScore=arcpy.da.InsertCursor("NDVITable.dbf", ("MeanScore"))
-Mean=arcpy.da.SearchCursor("NDVITable.dbf", ("MEAN"))
-for x in Mean:
-    if (x[0] >= 0.0 and x[0] <= 0.06):
-        meanScore.append(1)
-    elif (x[0] >= 0.061 and x[0] <= 0.115):
-        meanScore.append(2)
-    elif (x[0] >= 0.116 and x[0] <= 0.17):
-        meanScore.append(3)
-    elif (x[0] >= 0.171):
-        meanScore.append(4)
-        print "Done."
+#meanScore=arcpy.da.InsertCursor("NDVITable.dbf", ("MeanScore"))
+
+with arcpy.da.UpdateCursor("NDVITable.dbf", ("MEAN","Mean_Scr")) as Mean:
+
+    for x in Mean:
+        if (x[0] >= 0.0 and x[0] <= 0.06):
+            x[1] = 1
+        elif (x[0] > 0.06 and x[0] <= 0.115):
+            x[1] = 2
+        elif (x[0] > 0.115 and x[0] <= 0.17):
+            x[1] = 3
+        elif (x[0] > 0.17):
+            x[1] = 4
+            
+        Mean.updateRow(x)
+
+del Mean, x
+
+with arcpy.da.UpdateCursor("NDVITable.dbf", ("NumBus","Bus_Scr")) as Bus:
+
+    for x in Bus:
+        if (x[0] >= 0.0 and x[0] <= 200.0):
+            x[1] = 1
+        elif (x[0] > 200.0 and x[0] <= 800.0):
+            x[1] = 2
+        elif (x[0] > 800.0 and x[0] <= 1597.0):
+            x[1] = 3
+        elif (x[0] > 1597.0):
+            x[1] = 4
+            
+        Bus.updateRow(x)
         
+del Bus, x
+
+with arcpy.da.UpdateCursor("NDVITable.dbf", ("NumSchools","Schl_Scr")) as School:
+
+    for x in School:
+        if (x[0] == 0.0):
+            x[1] = 0
+        if (x[0] > 0.0 and x[0] <= 125.0):
+            x[1] = 1
+        elif (x[0] > 125.0 and x[0] <= 250.0):
+            x[1] = 2
+        elif (x[0] > 250.0 and x[0] <= 375.0):
+            x[1] = 3
+        elif (x[0] > 375.0):
+            x[1] = 4
+            
+        School.updateRow(x)
+        
+del School, x
+
+with arcpy.da.UpdateCursor("NDVITable.dbf", ("Mean_Scr","Bus_Scr","Schl_Scr", "Final_Scr")) as Final:
+
+    for x in Final:
+        x[3] = (3*(x[0]))+(2*(x[1]))+(x[2])
+            
+        Final.updateRow(x)
+        
+del Final, x
+        
+#Copy and clean finaltable
+
+arcpy.Copy_management("NDVITable.dbf", "IdealHome.dbf")
+
+arcpy.DeleteField_management("IdealHome.dbf", "Mean_Scr")
+arcpy.DeleteField_management("IdealHome.dbf", "Bus_Scr")
+arcpy.DeleteField_management("IdealHome.dbf", "Schl_Scr")
 
 print(">> End of Script <<")
 
